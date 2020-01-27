7d2a182965347e585d8f0242ee58db726469a33e
Lucas Marcos <lucas.marcos@multi24h.com.br>
2020-01-27 17:01:22 -0300

    Teste de Ionic

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,19 +1,16 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { DetailPage } from "../detail/detail";
 
 @Component({
   selector: 'page-list',
   templateUrl: 'list.html'
 })
 export class ListPage {
-  selectedItem: any;
   icons: string[];
   items: Array<{title: string, note: string, icon: string}>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
-
     // Let's populate this page with some filler content for funzies
     this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
     'american-football', 'boat', 'bluetooth', 'build'];
@@ -29,8 +26,8 @@ export class ListPage {
   }
 
   itemTapped(event, item) {
-    // That's right, we're pushing to ourselves!
-    this.navCtrl.push(ListPage, {
+    // That's right, we're pushing to Detail!
+    this.navCtrl.push(DetailPage, {
       item: item
     });
   }
